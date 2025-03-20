69cd2725cb15a891819a4b3c3e5b598f6cada5c9
cheickh-03 <cheickh.keita@hotmail.com>
2025-03-20 12:49:43 +0000

    update 16

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import ThemeProvider from "./context/ThemeContext.tsx";
-import { registerServiceWorker } from "./utils/serviceWorkerRegistration.ts";
 
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
 
 const basename = import.meta.env.BASE_URL;
 
-// Enregistrer le service worker
-registerServiceWorker();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter basename={basename}>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
